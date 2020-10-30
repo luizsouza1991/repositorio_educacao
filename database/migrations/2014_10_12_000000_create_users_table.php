c1647eddaa812a5616f1e9aa7e48a7946502d721
luiz <luiz@flexpeak.com.br>
2020-10-29 23:29:49 -0400

    Alterando código da tela de cadastro

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('data_nascimento');
+            $table->string('formacao');
+            $table->string('curso_formacao');
+            $table->string('instituicao');
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,65 @@
                 <div class="card-header"> Cadastrar Usuário</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    {!! Form::open(['method' => 'POST', 'route' => ['register']]) !!}
+                        @csrf
+                        <div class="form-group row">
+                            {!! Form::label('name', 'Name', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
+                            <div class="col-md-6">
+                                {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            {!! Form::label('data_nascimento', 'Data de Nascimento', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
+                            <div class="col-md-6">
+                                {!! Form::date('data_nascimento', null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            {!! Form::label('formacao', 'Formação', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
+                            <div class="col-md-6">
+                                {!! Form::select('formacao', [
+                                    'ensino_medio' => 'Ensino Médio',
+                                    'ensino_profissionalizante' => 'Ensino Profissionalizante',
+                                    'superior_completo' => 'Ensino Superior Completo',
+                                    'superior_incompleto' => 'Ensino Superior Incompleto',
+                                    'especializacao' => 'Especialização',
+                                    'mestrado' => 'Mestrado',
+                                    'doutorado' => 'Doutorado'
+                                ], null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            {!! Form::label('email', 'E-mail', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
+                            <div class="col-md-6">
+                                {!! Form::email('email', null, ['class' => 'form-control', 'placeholder' => 'example@email.com']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            {!! Form::label('formacao', 'Formação', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
+                            <div class="col-md-6">
+                                {!! Form::text('formacao', null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            {!! Form::label('curso_formacao', 'Curso de Formação', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
+                            <div class="col-md-6">
+                                {!! Form::text('curso_formacao', null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                {!! Form::submit('Cadastrar', ['class' => 'btn btn-primary']) !!}
+                            </div>
+                        </div>
+                    {!! Form::close() !!}
+                    {{-- <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
@@ -102,7 +160,7 @@
                                 </button>
                             </div>
                         </div>
-                    </form>
+                    </form> --}}
                 </div>
             </div>
         </div>
